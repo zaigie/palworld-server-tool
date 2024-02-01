edc9d97ddf313ef0c16fbfad053d4c6cfcb59b86
songzhibin97 <718428482@qq.com>
2024-02-01 19:29:24 +0800

    Update router.go

--- api/router.go
+++ api/router.go
@@ -1,6 +1,10 @@
 package api
 
 import (
+	"fmt"
+	"strings"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -17,8 +21,29 @@ type ErrorResponse struct {
 
 type EmptyResponse struct{}
 
+func Logger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		if !strings.HasPrefix(param.Path, "/swagger/") && !strings.HasPrefix(param.Path, "/assets/") {
+			// your custom format
+			return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+				param.ClientIP,
+				param.TimeStamp.Format(time.RFC1123),
+				param.Method,
+				param.Path,
+				param.Request.Proto,
+				param.StatusCode,
+				param.Latency,
+				param.Request.UserAgent(),
+				param.ErrorMessage,
+			)
+		}
+		return ""
+	})
+}
+
 func RegisterRouter() *gin.Engine {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(Logger(), gin.Recovery())
 
 	r.POST("/api/login", loginHandler)
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
