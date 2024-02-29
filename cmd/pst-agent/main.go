7d182b83bf229f33b73eca2509bfa591cf77bfc7
zaigie <jokerwho@yeah.net>
2024-02-29 23:39:02 +0800

    feat: pst-agent download .zip

--- cmd/pst-agent/main.go
+++ cmd/pst-agent/main.go
@@ -3,52 +3,70 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"sort"
 	"strconv"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-co-op/gocron/v2"
 	"github.com/google/uuid"
 	"github.com/spf13/viper"
+	"github.com/zaigie/palworld-server-tool/internal/logger"
+	"github.com/zaigie/palworld-server-tool/internal/system"
 )
 
 var (
-	port int
-	file string
+	port     int
+	savedDir string
 )
 
 func main() {
 	flag.IntVar(&port, "port", 8081, "port")
-	flag.StringVar(&file, "f", "", "Level.sav file location")
+	flag.StringVar(&savedDir, "d", "", "Directory containing Level.sav file")
 	flag.Parse()
 
-	viper.BindEnv("sav_file", "SAV_FILE")
-
+	viper.BindEnv("saved_dir", "SAVED_DIR")
 	viper.SetDefault("port", port)
-	viper.SetDefault("sav_file", file)
+	viper.SetDefault("saved_dir", savedDir)
+	savedDir = viper.GetString("saved_dir")
+
+	savDir, err := system.GetSavDir(savedDir)
+	if err != nil {
+		logger.Errorf("Failed to get directory include Level.sav: %v\n", err)
+		os.Exit(1)
+	}
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
 	r.GET("/sync", func(c *gin.Context) {
 		uuid := uuid.New().String()
-		cacheDir := filepath.Join(os.TempDir(), "pst", uuid)
+		cacheDir := filepath.Join(os.TempDir(), "pst-agent", uuid)
 		os.MkdirAll(cacheDir, os.ModePerm)
+		defer os.RemoveAll(cacheDir)
 
-		destFile := filepath.Join(cacheDir, "Level.sav")
-		copyStatus := copyFile(viper.GetString("sav_file"), destFile)
-		if !copyStatus {
+		err := system.CopyDir(savDir, cacheDir)
+		if err != nil {
+			logger.Errorf("Failed to copy directory: %v\n", err)
 			c.Redirect(http.StatusFound, "/404")
 			return
 		}
 
-		c.File(destFile)
+		zipFilePath := filepath.Join(os.TempDir(), "pst-agent", uuid+".zip")
+		err = system.ZipDir(cacheDir, zipFilePath)
+		if err != nil {
+			logger.Errorf("Failed to create zip: %v\n", err)
+			c.Redirect(http.StatusFound, "/404")
+			return
+		}
+
+		c.Header("Content-Disposition", "attachment; filename=sav.zip")
+		c.File(zipFilePath)
 	})
 
 	s, err := gocron.NewScheduler()
@@ -57,65 +75,60 @@ func main() {
 	}
 	_, err = s.NewJob(
 		gocron.DurationJob(60*time.Second),
-		gocron.NewTask(limitCacheFiles, filepath.Join(os.TempDir(), "pst"), 5),
+		gocron.NewTask(limitCacheFiles, filepath.Join(os.TempDir(), "pst-agent"), 5),
 	)
 	if err != nil {
 		fmt.Println(err)
 	}
 	s.Start()
 
-	fmt.Println("pst-agent is running, Listening on port", port)
+	logger.Infof("PST-Agent Listening on port %d\n", port)
 
-	r.Run(":" + strconv.Itoa(port))
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := r.Run(":" + strconv.Itoa(port)); err != nil {
+			logger.Errorf("Failed to start agent: %v\n", err)
+		}
+	}()
+
+	<-sigChan
+
+	logger.Info("PST-Agent gracefully stopped\n")
+
 }
 
-func copyFile(src, dst string) bool {
-	source, err := os.Open(src)
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-	defer source.Close()
-
-	destination, err := os.Create(dst)
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-	defer destination.Close()
-
-	_, err = io.Copy(destination, source)
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-	return true
-}
-
-// limitCacheFiles keeps only the latest `n` files in the cache directory
+// limitCacheFiles keeps only the latest `n` zip archives in the cache directory
 func limitCacheFiles(cacheDir string, n int) {
 	files, err := os.ReadDir(cacheDir)
 	if err != nil {
-		log.Println("Error reading cache directory:", err)
+		logger.Errorf("Error reading cache directory: %v\n", err)
 		return
 	}
 
-	if len(files) <= n {
+	zipFiles := []os.DirEntry{}
+	for _, file := range files {
+		if filepath.Ext(file.Name()) == ".zip" {
+			zipFiles = append(zipFiles, file)
+		}
+	}
+
+	if len(zipFiles) <= n {
 		return
 	}
 
-	sort.Slice(files, func(i, j int) bool {
-		infoI, _ := files[i].Info()
-		infoJ, _ := files[j].Info()
+	sort.Slice(zipFiles, func(i, j int) bool {
+		infoI, _ := zipFiles[i].Info()
+		infoJ, _ := zipFiles[j].Info()
 		return infoI.ModTime().After(infoJ.ModTime())
 	})
 
-	// Delete files that exceed the limit
-	for i := n; i < len(files); i++ {
-		path := filepath.Join(cacheDir, files[i].Name())
-		err = os.RemoveAll(path)
+	for i := n; i < len(zipFiles); i++ {
+		path := filepath.Join(cacheDir, zipFiles[i].Name())
+		err := os.Remove(path)
 		if err != nil {
-			fmt.Println("delete files path", path, err)
+			logger.Errorf("Failed to delete excess zip file: %v\n", err)
 		}
 	}
 }
