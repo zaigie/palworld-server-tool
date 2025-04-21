1e2a9626da776c932d522bde7b2e609048d3efb2
ventra007 <39557783+ventra007@users.noreply.github.com>
2025-04-21 22:41:23 +0200

    Update config.go

--- internal/tool/config.go
+++ internal/tool/config.go
@@ -9,24 +9,31 @@ import (
 
 // GetServerConfig reads the PalWorld server configuration file
 func GetServerConfig() (string, error) {
-    // Determine the path to the configuration file
-    savePath := viper.GetString("save.path")
+    // Get the explicit config path from the manage section
+    configPath := viper.GetString("manage.config_path")
     
-    // The configuration file is typically in a subdirectory of the save path
-    var configPath string
+    // If config_path is not specified, try to determine it from save.path
+    if configPath == "" {
+        savePath := viper.GetString("save.path")
+        
+        // Check if we're on Windows or Linux
+        windowsConfigPath := filepath.Join(savePath, "Config", "WindowsServer", "PalWorldSettings.ini")
+        linuxConfigPath := filepath.Join(savePath, "Config", "LinuxServer", "PalWorldSettings.ini")
+        
+        // Try Windows path first
+        if _, err := os.Stat(windowsConfigPath); err == nil {
+            configPath = windowsConfigPath
+        } else if _, err := os.Stat(linuxConfigPath); err == nil {
+            // Then try Linux path
+            configPath = linuxConfigPath
+        } else {
+            // If neither exists, return an error
+            return "", err
+        }
+    }
     
-    // Check if we're on Windows or Linux
-    windowsConfigPath := filepath.Join(savePath, "Config", "WindowsServer", "PalWorldSettings.ini")
-    linuxConfigPath := filepath.Join(savePath, "Config", "LinuxServer", "PalWorldSettings.ini")
-    
-    // Try Windows path first
-    if _, err := os.Stat(windowsConfigPath); err == nil {
-        configPath = windowsConfigPath
-    } else if _, err := os.Stat(linuxConfigPath); err == nil {
-        // Then try Linux path
-        configPath = linuxConfigPath
-    } else {
-        // If neither exists, return an error
+    // Check if the config file exists
+    if _, err := os.Stat(configPath); err != nil {
         return "", err
     }
     
@@ -41,24 +48,31 @@ func GetServerConfig() (string, error) {
 
 // SaveServerConfig writes to the PalWorld server configuration file
 func SaveServerConfig(content string) error {
-    // Determine the path to the configuration file
-    savePath := viper.GetString("save.path")
+    // Get the explicit config path from the manage section
+    configPath := viper.GetString("manage.config_path")
     
-    // The configuration file is typically in a subdirectory of the save path
-    var configPath string
+    // If config_path is not specified, try to determine it from save.path
+    if configPath == "" {
+        savePath := viper.GetString("save.path")
+        
+        // Check if we're on Windows or Linux
+        windowsConfigPath := filepath.Join(savePath, "Config", "WindowsServer", "PalWorldSettings.ini")
+        linuxConfigPath := filepath.Join(savePath, "Config", "LinuxServer", "PalWorldSettings.ini")
+        
+        // Try Windows path first
+        if _, err := os.Stat(windowsConfigPath); err == nil {
+            configPath = windowsConfigPath
+        } else if _, err := os.Stat(linuxConfigPath); err == nil {
+            // Then try Linux path
+            configPath = linuxConfigPath
+        } else {
+            // If neither exists, return an error
+            return err
+        }
+    }
     
-    // Check if we're on Windows or Linux
-    windowsConfigPath := filepath.Join(savePath, "Config", "WindowsServer", "PalWorldSettings.ini")
-    linuxConfigPath := filepath.Join(savePath, "Config", "LinuxServer", "PalWorldSettings.ini")
-    
-    // Try Windows path first
-    if _, err := os.Stat(windowsConfigPath); err == nil {
-        configPath = windowsConfigPath
-    } else if _, err := os.Stat(linuxConfigPath); err == nil {
-        // Then try Linux path
-        configPath = linuxConfigPath
-    } else {
-        // If neither exists, return an error
+    // Check if the config file exists
+    if _, err := os.Stat(configPath); err != nil {
         return err
     }
     
